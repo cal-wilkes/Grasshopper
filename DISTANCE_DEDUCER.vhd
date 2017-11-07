b4ba6023232fc3af69f94c7ffcc381dc99de0928
cal-wilkes <robert.wilkes@ucalgary.ca>
2017-11-06 21:47:31 -0700

    Update DISTANCE_DEDUCER.vhd

--- DISTANCE_DEDUCER.vhd
+++ DISTANCE_DEDUCER.vhd
@@ -28,9 +28,9 @@ entity DISTANCE_DEDUCER is
 	port(   
 	       CLK: in STD_LOGIC;
 	       RESET: in STD_LOGIC;
-	       COMPARE: in STD_LOGIC;
+	       COMPARE: in STD_LOGIC;               -- signal from ADC circuit that signals that the measure value correlates to the value read by the IR sensor
 	       REF:  in INTEGER;
-	       MEAS: in INTEGER;
+	       MEAS: in INTEGER;                    -- incoming ADC value from saw wave (DAC) module
 	       DISTANCE_RAW: out INTEGER;
 	       DISTANCE_D: out INTEGER;
 	       DISTANCE_X: out INTEGER   );
@@ -49,7 +49,7 @@ begin
           
                             if (COMPARE = '0') then 
                                         
-                                        i_DISTANCE_RAW <= MEAS;
+                                        i_DISTANCE_RAW <= MEAS;        -- raw unprocessed ADC value
                                         i_DISTANCE_X <= (MEAS)+REF;   -- example distance mathematical expression for cm REQUIRES CALIBRATION
                                         i_DISTANCE_D <= (MEAS)+REF; -- example distance mathematical expression for mm REQUIRES CALIBRATION
                                         
@@ -63,7 +63,7 @@ begin
 
                            if(rising_edge(CLK)) then 
                                         
-                                        DISTANCE_RAW <= i_DISTANCE_RAW;
+                                        DISTANCE_RAW <= i_DISTANCE_RAW;       -- update signals
                                         DISTANCE_X <= i_DISTANCE_X;
                                         DISTANCE_D <= i_DISTANCE_D;
                           
