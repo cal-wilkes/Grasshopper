b931318d24d92d9c97c7c439a5aeba2670d330fa
cal-wilkes <robert.wilkes@ucalgary.ca>
2017-11-06 22:19:06 -0700

    Update ADJUST_LEVEL.vhd

--- ADJUST_LEVEL.vhd
+++ ADJUST_LEVEL.vhd
@@ -1,3 +1,25 @@
+--This module is responsible for controlling the distance measurement at which the solenoid control module should actuate the solenoid.
+--The module takes input from two buttons to either increase or decrease the module's output integer LEVEL. The increments between levels
+--should be equivalent to approximately 1cm intervals from 4cm to 10cm. The values of these increments shall be determined using the calibration 
+--signal REFERENCE and a mathematical formulation to determine all other level measurements from the reference distance. Essentially, implementation 
+--requires a Moore FSM which transitions between states equivalent to various levels of difficulty. I would suggest beginning work on this module 
+--by referring to the code for the frequency_module and amplitude_module from lab 4. Please use if statements to replace the case statements as I 
+--have found that if statements synthesize more reliably. Additionally designing a FSM on paper will be beneficial for designing this module and should 
+--be completed with the intention that it may be used in the final report. 
+
+--Inputs: 
+--		CLK: STD_LOGIC 
+--		RESET: STD_LOGIC 
+--		LEVEL_UP_BTN: INTEGER
+--		LEVEL_DOWN_BTN: INTEGER 
+--		REFERENCE: INTEGER . 
+
+--Outputs: 
+--		LEVEL: INTEGER
+
+
+
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
