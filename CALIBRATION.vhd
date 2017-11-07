99ab5b78fc243c296fdae4222b284a6a45315ae1
cal-wilkes <robert.wilkes@ucalgary.ca>
2017-11-06 22:20:54 -0700

    Update CALIBRATION.vhd

--- CALIBRATION.vhd
+++ CALIBRATION.vhd
@@ -1,4 +1,13 @@
---This module's functionality is extremely important for consistent functioning of our VHDL code and system. The module will function as a state machine where in the first state (S0) the code waits for the signal from the input CALIBRATE to equal 1. This occurs only when the Calibrate Button on the FPGA is pressed. Once in the second state (S1) the code will sample and hold the current value of the input DISTANCE_X and store the value in a temporary storage signal internal to the module. Finally in the final code will update the code should wait a predetermined number of clock cycles (using an internal counter or an instantiation of the down counter module) before updating the output signal REFERENCE with the value store in the internal temporary signal. The value of REFERENCE will correspond to the voltage output of the ADC at a predetermined distance of for example 10cm. The signal REFERENCE will be used in other modules to determine the mathematical translation of the ADC value to a measurement of distance in centimetres. 
+--This module's functionality is extremely important for consistent functioning of our VHDL code and system. 
+--The module will function as a state machine where in the first state (S0) the code waits for the signal from 
+--the input CALIBRATE to equal 1. This occurs only when the Calibrate Button on the FPGA is pressed. Once in 
+--the second state (S1) the code will sample and hold the current value of the input DISTANCE_X and store the value 
+--in a temporary storage signal internal to the module. Finally in the final code will update the code should wait a
+--predetermined number of clock cycles (using an internal counter or an instantiation of the down counter module) 
+--before updating the output signal REFERENCE with the value store in the internal temporary signal. The value of 
+--REFERENCE will correspond to the voltage output of the ADC at a predetermined distance of for example 10cm. The signal
+--REFERENCE will be used in other modules to determine the mathematical translation of the ADC value to a measurement of
+--distance in centimetres. 
 
 
 
