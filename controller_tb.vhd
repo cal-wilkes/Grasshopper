caa6bdd0380cb3795bcd921537c4b4278633e5ef
lawrencechiu12 <31356619+lawrencechiu12@users.noreply.github.com>
2017-11-28 14:08:53 -0700

    edit

--- controller_tb.vhd
+++ controller_tb.vhd
@@ -4,7 +4,7 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 
-entity of controller_tb is
+entity controller_tb is
 end controller_tb;
 
 
@@ -15,6 +15,7 @@ port( clk: in STD_LOGIC;
                 BUTTON: in STD_LOGIC_VECTOR(1 downto 0);
                 SWITCH: in STD_LOGIC_VECTOR(2 downto 0);
                 COMPARE: in STD_LOGIC;
+                CHANGE: in std_logic;
                 SAW_PWM: out STD_LOGIC;
                 r: out STD_LOGIC_VECTOR(3 downto 0);
                 g: out STD_LOGIC_VECTOR(3 downto 0);
@@ -28,9 +29,10 @@ end component;
 
 
 signal clk: std_logic;
-signal button: std_logic_vector(1 downto 0);
-signal switch: std_logic_vector(2 downto 0);
-signal compare: std_logic;
+signal change: std_logic := '0';
+signal button: std_logic_vector(1 downto 0) := "00";
+signal switch: std_logic_vector(2 downto 0) := "000";
+signal compare: std_logic := '0';
 
 signal saw_pwm: std_logic;
 signal r: std_logic_vector(3 downto 0);
@@ -49,6 +51,7 @@ begin
 				button => button,
 				switch => switch,
 				compare => compare,
+				change => change,
 				saw_pwm => saw_pwm,
 				r => r,
 				b => b,
@@ -91,6 +94,10 @@ begin
 		compare <= '0';
 		wait for 100 ns;
 		compare <= '1';
+		wait for 100 ns;
+		change <= '0';
+		wait for 100 ns;
+		change <= '1';
 		wait for 100 ns;
 		wait;
 	end process;
