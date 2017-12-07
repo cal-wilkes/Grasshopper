440d0ee0561e34d5af3fc0debfa5d02ba43039dd
dangdiana <33231024+dangdiana@users.noreply.github.com>
2017-12-07 10:34:13 -0700

    Update CALIBRATION_TB.vhd

--- CALIBRATION_TB.vhd
+++ CALIBRATION_TB.vhd
@@ -1,38 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11/05/2017 11:40:06 AM
--- Design Name: 
--- Module Name: DISTANCE_DEDUCE_TB - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity CALIBRATION_TB is
---  Port ( );
 end CALIBRATION_TB;
 
 architecture Behavioral of CALIBRATION_TB is
